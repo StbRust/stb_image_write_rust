fab090d2d8285c366459118506c6c4aa24ba3d6d
Roman Shapiro <netframeworker@gmail.com>
2022-01-04 08:07:23 +0700

    Various fixes

--- samples/src/main.rs
+++ samples/src/main.rs
@@ -6,6 +6,87 @@ use std::io::{prelude::*, Cursor};
 
 use std::time::Instant;
 
+pub struct ImageWriter {
+    pub file: File,
+}
+
+impl ImageWriter {
+    fn new(path: &str) -> ImageWriter {
+        ImageWriter {
+            file: File::create(path).expect("could not create file"),
+        }
+    }
+
+    pub fn write(&mut self, data: *mut u8, size: i32) {
+        let mut arr = vec![0; size as usize];
+
+        unsafe {
+            std::ptr::copy_nonoverlapping(data, arr.as_mut_ptr(), size as usize);
+            self.file.write_all(&arr);
+        }
+    }
+
+    unsafe fn write_png(&mut self, x: i32, y: i32, comp: i32, data: *mut u8) {
+        stb_image_write_rust::stbi_write_png_to_func(
+            ImageWriter_func,
+            (self as *mut ImageWriter) as *mut u8,
+            x,
+            y,
+            comp,
+            data,
+            x * comp,
+        );
+    }
+
+    unsafe fn write_jpg(&mut self, x: i32, y: i32, comp: i32, data: *mut u8, quality: i32) {
+        stb_image_write_rust::stbi_write_jpg_to_func(
+            ImageWriter_func,
+            (self as *mut ImageWriter) as *mut u8,
+            x,
+            y,
+            comp,
+            data,
+            quality,
+        );
+    }
+}
+
+fn ImageWriter_func(context: *mut u8, data: *mut u8, size: i32) {
+    let mut writer = context as *mut ImageWriter;
+    unsafe {
+        (*writer).write(data, size);
+    }
+}
+
 fn main() {
-    let k = 5;
+    // Load file into memory
+    let path = r"D:\Temp\CharacterControllerSample.jpg";
+    let mut f = File::open(path).expect("file not found");
+    let mut contents = vec![];
+    f.read_to_end(&mut contents);
+
+    // Load the image
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut comp: i32 = 0;
+    let img: *mut u8;
+
+    unsafe {
+        img = stb_image_rust::stbi_load_from_memory(
+            contents.as_mut_ptr(),
+            contents.len() as i32,
+            &mut x,
+            &mut y,
+            &mut comp,
+            stb_image_rust::STBI_rgb_alpha,
+        );
+
+        // Do something with it
+        let outputPath = r"D:\Temp\CharacterControllerSample2.jpg";
+        let mut writer = ImageWriter::new(outputPath);
+        writer.write_jpg(x, y, 4, img, 90);
+
+        // Free the allocated memory
+        stb_image_rust::c_runtime::free(img);
+    }
 }
--- stb_image_write_rust/src/stb_image_write.rs
+++ stb_image_write_rust/src/stb_image_write.rs
@@ -8,14 +8,14 @@ pub unsafe fn stbiw__writefv(mut s: *mut stbi__write_context, fmt: &str, args: &
         if c == ' ' {
         } else if c == '1' {
             let mut x: u8 = (args[argIndex] & 0xff) as u8;
-            (*(*s).func)((*s).context, ((&mut x) as *mut u8), 1);
+            ((*s).func)((*s).context, ((&mut x) as *mut u8), 1);
             argIndex += 1;
         } else if c == '2' {
             let mut x: i32 = args[argIndex];
             let mut b: [u8; 2] = [0; 2];
             b[(0) as usize] = (((x) & 0xff) as u8);
             b[(1) as usize] = (((x >> 8) & 0xff) as u8);
-            (*(*s).func)((*s).context, ((b.as_mut_ptr()) as *mut u8), 2);
+            ((*s).func)((*s).context, ((b.as_mut_ptr()) as *mut u8), 2);
             argIndex += 1;
         } else if c == '4' {
             let mut x: u32 = args[argIndex] as u32;
@@ -24,7 +24,7 @@ pub unsafe fn stbiw__writefv(mut s: *mut stbi__write_context, fmt: &str, args: &
             b[(1) as usize] = (((x >> 8) & ((0xff) as u32)) as u8);
             b[(2) as usize] = (((x >> 16) & ((0xff) as u32)) as u8);
             b[(3) as usize] = (((x >> 24) & ((0xff) as u32)) as u8);
-            (*(*s).func)((*s).context, ((b.as_mut_ptr()) as *mut u8), 4);
+            ((*s).func)((*s).context, ((b.as_mut_ptr()) as *mut u8), 4);
             argIndex += 1;
         } else {
             return;
